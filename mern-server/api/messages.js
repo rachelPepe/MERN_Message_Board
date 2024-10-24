46cc698e17b1cd776279d892cdbfaabb95703504
rachelpepe <rachelmpepe@gmail.com>
2024-10-24 14:38:09 -0700

    changed to async functions without callbacks on api router functions

--- mern-server/api/messages.js
+++ mern-server/api/messages.js
@@ -7,106 +7,120 @@ const router = require("express").Router();  // Express router instance
 
 
 /* Route: Get list of all messages sorted by posted field in descending order 
-- Method: GET /api/messages
+- Method: GET request made to /api/messages - When a request is made to http://localhost:8000/api/messages, this function gets executed.
 - "/" stands for root path (/api/messages)
--if succesful, returns list of messages in JSON format  -if error, returns 400 status with error */
-router.get("/", function(req, res){
-    Message.find().sort("-posted").exec(function(err, messages){
-        if (err) {
-            res.statusCode(400).send(err);
-        }
-        else {
-            res.json(messages);
-        }
-    });
+- Message.find(): Retrieves all documents (messages) from the MongoDB collection.
+- .sort("-posted"): Sorts the messages by the posted field in descending order (most recent messages first).
+- exec(): Executes the query and returns a Promise. With await, we pause the function execution until the Promise resolves.
+- If the query is successful, the messages variable will hold an array of message objects which is sent back to the client as JSON (res.json(message))
+- if error occurs, catch block executes and sends 400 status with error details back to client
+*/
+router.get("/", async function(req, res){
+    try {
+        const messages = await Message.find().sort("-posted").exec();
+        res.json(messages);
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 
 
 /* Route: Get a Message by ID - finds message by ID provided in URL
-- Method: GET /api/messages/:id
+- Method: GET request made to /api/messages/:id - When a request is made to http://localhost:8000/api/messages/:id, this function gets executed.
 - "/:id" is a route parameter (/api/messages/:id)
-- The Query.exec() method is called last to execute the query and provides a callback function that is called after the query executes.
-- if found - returns message in JSON format 
-- if not found - returns 404 status
-- if error - returns 400 status with the error
+- req.params holds any route parameters passed by the client - Ex: If the request is GET /api/messages/634e7b3a6d0f88..., then req.params.id will be "634e7b3a6d0f88...".
+- Message.findById(req.params.id) mongoose query that tries to find message by id, if found returns message document in JSON format to client
+- if message doesnt exist, 404 status code sent
+- catch block executes if error occurs 
 */
-router.get("/:id", function(req, res) {
-    Message.findById(req.params.id, function(err, message) {
-        if (err){
-            res.status(400).send(err)
-        }
-        else if (message === null){
-            res.sendStatus(404);
+router.get("/:id", async function(req, res) { // 
+    try {
+        const message = await Message.findById(req.params.id).exec();
+        if (!message){
+            res.sendStatus(404)
         }
         else {
-            res.json(message)
+            res.json(message);
         }
-    });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 
 
 /* Route Add a new message to the database using the data from request body and the Message model 
-- Method: POST /api/messages
-- if succesful saves the message to the database and returns it with 201 status
+- Method: POST route that listens on /api/messages - If the client sends a POST request to /api/messages, this function will run.
+- Asynchronous function: Since we’re dealing with database operations, we use async/await to handle the Promises cleanly.
+- new Message(req.body): Creates a new Mongoose model instance using the client’s data that was sent in the request body
+- const savedMessage = await message.save(): - await pauses the function until the save operation completes, and the saved message is stored in savedMessage.
+- res.status.apply(201).json(savedMessage): - sends http 201 created status code, indicationg new resource created, json(savedMesage) sends the saved message to the client in json
 - if error returns 400 status with error
 */
-router.post("/", function(req, res) {
-    const message = new Message(req.body);
-    message.save(function(err, message) {
-        if (err){
-            res.status(400).send(err);
-        }
-        else {
-            res.status(201).json(message);
-        }
-    });
+router.post("/", async function(req, res) {
+    try {
+        const message = new Message(req.body);
+        const savedMessage = await message.save();
+        res.status.apply(201).json(savedMessage);
+    }
+    catch {
+        res.status(400).send(err);
+    }
 });
 
 
 
 /* Route update an existing message by its ID using the data in request body
-- Method: PUT api/message/:id
+- Method: PUT route that listens on api/message/:id 
 - runValidators: true ensures that all fields still meet the schema requirements.
-- if succesful returns 204 status(no content
+- if succesful returns 204 status(no content)
 - if message not found - returns 404 status
 - if error - returns 400 status and error
 */
-router.put("/:id", function(req, res){
-    const messagePart = req.body;
-    Message.updateOne( { _id: req.params.id }, messagePart, { runValidators: true }, function(err, result) {
-        if (err){
-            res.status(400).send(err);
-        }
-        else if (result.matchedCount === 0){
-            res.sendStatus(404);
+router.put("/:id", async function(req, res){
+    try {
+        const messagePart = req.body;
+        result = await Message.updateOne( { _id: req.params.id }, messagePart, { runValidators: true });
+        if (result.matchedCount === 0){
+            res.sendStatus(404); // not found
         }
         else {
-            res.sendStatus(204);
+            res.sendStatus(204); // succesfull no content
         }
-    });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+        
+    
 });
 
 
 
 /* Route delete message by id using request body
+- Method: DELETE route that listens on /api/messages/:id - If the client sends a DELETE request to /api/messages/652f2f41c72d5c68b4f3c5c2, the function will attempt to delete the 
+        message with that ID.
+- req.params.id: Extracts the ID from the request URL (e.g., /api/messages/652f2f41c72d5c68b4f3c5c2).
 - if succesful returns 404 status (no content)
 - if message not found returns 404 status
 - if error occurs returns 400 status with error 
 */
-router.delete("/:id", function(req, res) {
-    Message.deleteOne({ _id: req.params.is }, function(err, result) {
-        if (err) {
-            res.status(400).send(err);
-        }
-        else if (result.matchedCount === 0) {
-            res.sendStatus(404);
+router.delete("/:id", async function(req, res) {
+    try {
+        const result = await Message.deleteOne({ _id: req.params.id }) 
+        if (result.deletedCount === 0) {
+            res.sendStatus(404); // not found
         }
         else {
-            res.sendStatus(204)
+            res.sendStatus(204); // no content
         }
-    });
+    }
+    catch (err) {
+        res.status(400).send(err)
+    }
 });
 
 module.exports = router;
--- mern-server/message-board-backend.md
+++ mern-server/message-board-backend.md
@@ -13,3 +13,9 @@
 - (db.js) - This file handles the MongoDB database connection using Mongoose. creats message_db whcih will store and access documents
 
 - (message.js) - This file defines the Message model (schema) using Mongoose. The model represents the structure of a "message" document stored in the MongoDB database.
+
+
+** once i had made all files and tried to run founs out i had to use async instead of call back functions 
+- async/await makes the code cleaner and easier to read.
+- Mongoose operations now return Promises (no need for callbacks), which aligns with the latest JavaScript standards.
+- If an error occurs, we catch it in the try/catch block and send the appropriate response.
